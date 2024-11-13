254c8ff3b9146078c8e7eb3edd9e2def79ae730d
LÃeo TREMOUREUX <ltremoureux@hotmail.fr>
2024-11-13 18:06:29 +0100

    start of airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,35 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# 1. Clean the database 🗑️
+puts 'Cleaning database...'
+Flat.destroy_all
+
+
+# 2. Create the instances 🏗️
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Small',
+  address: 'Rennes',
+  description: 'A lovely flat',
+  price_per_night: 27,
+  number_of_guests: 34
+)
+Flat.create!(
+  name: 'Medium',
+  address: 'ST MALO',
+  description: 'Au top la vue',
+  price_per_night: 75,
+  number_of_guests: 7
+)
+Flat.create!(
+  name: 'BIGGY',
+  address: 'Pontivy',
+  description: 'Bienvenue à la campagne',
+  price_per_night: 153,
+  number_of_guests: 9
+)
